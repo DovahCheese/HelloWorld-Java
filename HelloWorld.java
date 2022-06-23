b9cf3851e6db019a4922e858025e210687882124
DovahCheese <mester.atle@gmail.com>
2022-06-23 19:14:45 +0200

    Update stuff
    Mostly experimenting

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,16 @@
+import javafx.scene.shape.Line;
+
 public class HelloWorld {
     private static String goodbye = "Goodbye World";
 
     private static String midlife = "This World is Nice";
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println(midlife);
-        System.out.println(goodbye);
+        LinesToPrint.lines.add("Hello World");
+        LinesToPrint.lines.add(midlife);
+        LinesToPrint.lines.add(goodbye);
+        LinesToPrint.createDefaultLines();
+        LinesToPrint.lines.add("Dairy");
+        LinesToPrint.printLines();
     }
 }
--- LinesToPrint.java
+++ LinesToPrint.java
@@ -4,12 +4,20 @@ public class LinesToPrint {
     public static List<String> lines = new ArrayList<String>();
 
     public static void main(String[] args) {
+        createDefaultLines();
+        printLines();
+    }
+
+    public static void createDefaultLines() {
         lines.add("Cheese");
         lines.add("Milk");
         lines.add("Butter");
 
+    }
+    public static void printLines() {
         for (String line : lines) {
             System.out.println(line);
+
         }
     }
 }
