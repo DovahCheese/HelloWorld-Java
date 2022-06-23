898acf62f78b6c22bae8c8c0f85678b0004de0b2
DovahCheese <mester.atle@gmail.com>
2022-06-23 18:43:07 +0200

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    private static String goodbye = "Goodbye World";
+
+    private static String midlife = "This World is Nice";
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(midlife);
+        System.out.println(goodbye);
+    }
+}
